d050295f06d0754a734000733086c5e32f4e6460
kstick9210 <kstick9210@outlook.com>
2021-03-09 12:04:46 -0600

    add burger menu package

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { slide as Menu } from "react-burger-menu";
 
 import "./Header.css";
 
 export default function Header() {
     return (
         <div className="header">
+            <Menu right >
+                <a to="/">Home</a>
+                <a to="/">About</a>
+                <a to="/">Products</a>
+                <a to="/">Contact Us</a>
+            </Menu>
             <h1>Header</h1>
         </div>
     )
